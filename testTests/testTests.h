95050d84e14933bec7cb2561b418b2c0a638e62c
desjason <des_jason@163.com>
2012-11-16 00:14:39 +0800

    3 commit

---
+++ testTests/testTests.h
@@ -0,0 +1,13 @@
+//
+//  testTests.h
+//  testTests
+//
+//  Created by Lu Cao on 12-11-13.
+//  Copyright (c) 2012年 Lu Cao. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface testTests : SenTestCase
+
+@end
---
+++ testTests/testTests.m
@@ -0,0 +1,32 @@
+//
+//  testTests.m
+//  testTests
+//
+//  Created by Lu Cao on 12-11-13.
+//  Copyright (c) 2012年 Lu Cao. All rights reserved.
+//
+
+#import "testTests.h"
+
+@implementation testTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in testTests");
+}
+
+@end
